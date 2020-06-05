c56d091c25b18cfea8b5b5e41f8ea070efa49c25
hrithikchandpk <60618995+hrithikchandpk@users.noreply.github.com>
2020-06-05 16:55:08 +0530

    Update pub.py

--- week_2/scripts/pub.py
+++ week_2/scripts/pub.py
@@ -2,13 +2,15 @@
 import rospy
 from std_msgs.msg import Float64
 
+
 if __name__ == "__main__":
 
     rospy.init_node("rad", anonymous = False)
     pub=rospy.Publisher('radius',Float64, queue_size=10)
-    radius=1.0
     rate = rospy.Rate(15)
+    rad=1.0
     while not rospy.is_shutdown(): 
-        pub.publish(radius)
+        pub.publish(rad)
         rate.sleep()  
+  
   
