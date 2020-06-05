c20c2cf6e332059cc53e1f16dfb6cf00c2edc74f
hrithikchandpk <60618995+hrithikchandpk@users.noreply.github.com>
2020-06-05 18:24:31 +0530

    Update bonus8.py

--- week_2/scripts/bonus8.py
+++ week_2/scripts/bonus8.py
@@ -5,33 +5,30 @@ from geometry_msgs.msg import Twist
 import math
 global count
 def excel_time(time):
-    return  time.secs / (24.0*60.0*60.0)
+    return  time.secs 
 
 def publ(pub,r):
-    
+    count=0
     t= rospy.get_rostime()
     p=excel_time(t)
     vel=Twist()
+    vel.linear.y=0.0
+    vel.linear.x=0.20   
+    vel.angular.x=0.0
+    vel.angular.y=0.0
+    print (r,p)
     
-    vel.linear.z=0
-    vel.angular.x=0
-    vel.angular.y=0
-    vel.angular.z=-1
-    if p-r<7:
-        count=0
-
-    if p-r %6.28==0.0 and count==0:
+    if (p-r)>6.28:
         count=1
-    elif p-r %6.28==0.0 and count==1:
-        count=0    
-    if  count==0:
-        vel.linear.x=1*math.cos(p-r)
-        vel.linear.y=1*math.sin(p-r)
+
+    if count==0:
         vel.angular.z=1
+
+
     elif count==1:
-        vel.linear.x=1*math.cos(p-r)
-        vel.linear.y=1*math.sin(r-p)
-        
+        vel.angular.z=-1
+
+                  
     pub.publish(vel) 
        
        
@@ -47,4 +44,5 @@ if __name__ == "__main__":
     while not rospy.is_shutdown(): 
         publ(pub,r)
         rate.sleep()  
+    
     
